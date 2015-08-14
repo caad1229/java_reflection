bc6d14b67de7e8c0c58a89b1296a2b3a2860bf22
S.S <caad1229@gmail.com>
2015-08-14 15:10:23 +0900

    added new class

---
+++ src/main/java/com/caad1229/testReflection/Refref.java
@@ -0,0 +1,8 @@
+package com.caad1229.testReflection;
+
+public class Refref {
+    private void    funcVoid    ()             { }
+    private String  funcString  (String str)   { return str; }
+    private boolean funcBoolean (boolean bool) { return bool; }
+    private int     funcInt     (int i)        { return i; }
+}
